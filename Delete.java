a931a76f6ff67df3caeb64bc493778c7c1d1fc22
reishka <rg.NCC1701D@gmail.com>
2013-02-10 20:04:22 -0500

    Done.

--- Delete.java
+++ Delete.java
@@ -2,13 +2,13 @@ import java.io.*;
 
 public class Delete {
   /* Command::
-     delet name           where name must be the name of a file and its paths.
+     delete name           where name must be the name of a file and its paths.
      Action:
-     delets name
+     deletes name
      Errors:
      The user enters:
-     1. delet                The user did not type the file name.
-     2. delet name           The file name does not exist in the path.
+     1. delete                The user did not type the file name.
+     2. delete name           The file name does not exist in the path.
      3. delete name          But the file name does is a directory.
      When there is an error println a message and return (not exit).
 */
@@ -17,5 +17,19 @@ public class Delete {
   }
 
   private void f(String name){
+	  
+	  if (name.equalsIgnoreCase(""))
+		  System.out.println("File Name not Specified");
+	  else
+	  {
+		  File file = new File(name);
+		  if(!file.exists())
+			  System.out.println("File does not exist.");
+		  	else if (file.isDirectory())
+			  System.out.println("File specified is a directory.");
+			else
+				file.delete();
+	  }
+	  
   }
 }
